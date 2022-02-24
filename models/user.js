63c7befd02ab9b91683e8a7972ee754b69603908
ynjnkn <yjun_k@hotmail.com>
2022-02-24 12:02:26 +0900

    3-4 로그인/회원가입 기능을 Sequelize로 구현하기(1) 완료

--- models/user.js
+++ models/user.js
@@ -1,14 +1,27 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  nickname: String,
-  password: String,
-});
-UserSchema.virtual("userId").get(function () {
-  return this._id.toHexString();
-});
-UserSchema.set("toJSON", {
-  virtuals: true,
-});
-module.exports = mongoose.model("User", UserSchema);
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    userId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    email: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
